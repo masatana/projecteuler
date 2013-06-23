f2a13efbd91d37b013df1663f6156673725eec98
masatana <plaza.tumbling@gmail.com>
2013-06-23 17:46:32 +0900

    solved problem38

--- problem35.py
+++ problem35.py
@@ -1,10 +1,44 @@
 # -*- coding: utf-8 -*-
 import functions
 
+def f(l):
+    tmp = 0
+    for i, j in enumerate(l):
+        tmp += j * (10 ** (len(l) - 1 - i))
+    return tmp
+
+def gen_circular_number(n):
+    new_n = [int(x) for x in list(str(n))]
+    for i in range(len(str(n))):
+        tmp = new_n.pop(0)
+        new_n.append(tmp)
+        yield f(new_n)
+
 l = []
 for prime in functions.gen_prime_numbers():
+    ff = True
     if prime > 1000000:
         break
-    l.append(prime)
+    if prime < 10:
+        l.append(prime)
+    else:
+        ll = [int(x) for x in list(str(prime))]
+        for dame in [0,2,4,5,6,8]:
+            if dame in ll:
+                ff = False
+                break
+        if ff:
+            l.append(prime)
 
-print 
+tmp = 0
+b = True
+for prime in l:
+    for circular_number in gen_circular_number(prime):
+        if circular_number not in l:
+            b = False
+            break
+    if b:
+        tmp += 1
+    cnt = 0
+    b = True
+print tmp
--- problem38.py
+++ problem38.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
 def check(n):
-    l = [x for x in list(str(n))]
+    l = [x for x in list(n)]
     if '0' in l:
         return False
-    elif len(set(l)) == 9:
+    elif set(l) == set(['1','2','3','4','5','6','7','8','9']) and len(l) == 9:
         return True
     else:
         return False
 
+for i in range(10000, 1, -1):
+    j = 0
+    n = '' 
+    while True:
+        j += 1
+        n += str(i * j)
+        if len(n) >= 9:
+            break
+    if check(n):
+        print n
+        break
