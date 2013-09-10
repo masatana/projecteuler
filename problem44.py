142691ab50bde464c91513682d4e31654028b1f2
masatana <plaza.tumbling@gmail.com>
2013-09-10 20:51:40 +0900

    now implementing

--- problem44.py
+++ problem44.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import itertools
 
+dic = {}
 def f(n):
     i = 1
     while True:
@@ -8,22 +9,34 @@ def f(n):
             break
         yield i * (3 * i - 1) / 2
         i += 1
-
 def g():
     i = 1
     while True:
         yield i * (3 * i - 1) / 2
         i += 1
 
-def check(n):
-    pass
+def h(n):
+    return n * (3 * n - 1) / 2
 
-dic = {}
+def check(n):
+    i = 1
+    while True:
+        c = 2 * n - 3 * i ** 2 + i
+        if  c == 0:
+            return True
+        elif c > 0:
+            return False
+        else:
+            i += 1
+
 
 for d in g():
     i = 1
+    print(d)
     while (3 * i - 2) < d:
         for j in range(1, i):
-            if check(f(i) + f(j)):
+            if check(dic.setdefault(i, h(i)) + dic.setdefault(j, h(j))):
                 print(d)
                 exit()
+            print(i, j)
+        i += 1
